ac56eaf000749410305ff7482b0cf5d2b798275a
Ziad Tareq <ziadtareqd22@gmail.com>
2025-11-17 03:24:40 +0200

    Jenkinsfile edits

--- jenkins/shared-library/vars/gitPushChanges.groovy
+++ jenkins/shared-library/vars/gitPushChanges.groovy
@@ -10,10 +10,12 @@ def call(String commitMessage, String branch = 'main', String credentialsId = 'g
                 # Configure Git
                 git config user.email "jenkins@clouddevops.local"
                 git config user.name "Jenkins CI"
-                
+    
+                # Checkout branch (add this)
+                git checkout ${branch} || git checkout -b ${branch}
+    
                 # Add changes
                 git add .
-                
                 # Check if there are changes to commit
                 if git diff --staged --quiet; then
                     echo "No changes to commit"
